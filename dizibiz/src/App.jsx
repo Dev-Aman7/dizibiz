68ad66b29f766326d5ed04b35dbd30ac364f39aa
Aman <a8285443826@gmail.com>
2020-08-19 15:39:16 +0530

    store configured

--- dizibiz/src/App.jsx
+++ dizibiz/src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./store";
+
 import Blog from "./Pages/Blog";
 import Post from "./Pages/Post";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/post" component={Post} />
-                <Route path="/" component={Blog} />
-            </Switch>
-        </BrowserRouter>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/post" component={Post} />
+                        <Route path="/" component={Blog} />
+                    </Switch>
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
     );
 }
 
